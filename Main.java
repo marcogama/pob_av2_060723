b8abe5397f3bcc6b18fd4b42d3c69b548c9ff0e5
Marco Antonio R. Gama <71337523+marcogama@users.noreply.github.com>
2023-07-10 19:46:56 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,55 @@ package pob_av2_060723;
 import java.util.*;
 public class Main {
 
-	public static void main(String[] args) {
+	public static int Busca(){
+
+		ArrayList<Integer> busca = new ArrayList<Integer>();
+		for(int i : busca){
+			if(i == id)
+				return id;
+		}
+	}
+	public static Alter(){
+
+		for(int i : altera){
+
+			if(i == id){
+				
+			}
+		}
+	}
+	public static Insere(){
+
+		for(int j : insere){
+
+			if(j == insere){
+				
+			} 
+		}
+	}
+	public static Exclui(){
+
+		for(int j : exclui){
+
+			if(j == exclui){
+				
+			}
+		}
+	}
+	public static Lista(){
+
+		for(int k : lista){
+
+			System.out.println(id + ": " );
+		}
+	}
+	static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Cama cama = new Cama();
+		Cliente cliente = new Cliente();
+		Quarto quarto = new Quarto();
+		Reserva reserva = new reserva();
 	}
 
 }

295bd47c8ee0e932e3a46cce251c94b12fa7dbc9
Marco Antonio R. Gama <71337523+marcogama@users.noreply.github.com>
2023-07-12 19:08:42 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 package pob_av2_060723;
 import java.util.*;
+import java.util.regex.*;
 public class Main {
 
 	public static int Busca(){
@@ -47,10 +48,80 @@ public class Main {
 	static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner digite = new Scanner(System.in);
 		Cama cama = new Cama();
 		Cliente cliente = new Cliente();
 		Quarto quarto = new Quarto();
 		Reserva reserva = new reserva();
+		
+		ArrayList<cama> = new ArrayList<cama>();
+		ArrayList<cliente> = new ArrayList<cliente>();
+		ArrayList<quarto> = new ArrayList<quarto>();
+		ArrayList<reserva> = new ArrayList<reserva>();
+
+		int op, opcao, i, j, k, l;
+		i = j = k = l = 1;
+		do{
+			System.out.println("\nMenu de Opcoes\n");
+			System.out.println("1 : Cliente");
+			System.out.println("2 : Quarto");
+			System.out.println("3 : Reserva");
+			System.out.println("4 : Cama");
+			System.out.println("5 : Sair");
+
+			op = digite.nextInt();
+			if(op < 1 || op > 5)
+				System.out.println("Opcao invalida");
+			if(op == 1){
+
+				System.out.println("\nMenu de Opcoes\n");
+				System.out.println("1 : Inserir Cliente");
+				System.out.println("2 : Remover Cliente");
+				System.out.println("3 : Listar Cliente");
+				System.out.println("4 : Alterar Cliente");
+				System.out.println("5 : Sair");
+
+				opcao = digite.nextInt();
+				if(opcao < 1 || opcao > 5)
+					System.out.println("Opcao invalida");
+				if(opcao == 1){
+
+					String Nome, End, Email, Pais, Num_pas;
+					int Cpf, Dt_nasc, Cep;
+					boolean ok;
+					
+					cliente.add("cliente" + i);
+					System.out.println("Digite os dados do cliente: nome, cpf, data de nascimento, endereco, cep,
+							   email, numero do passaporte, Pais");
+
+					Scanner nome = new Scanner(System.in);
+					Nome = nome.next();
+					Scanner end = new Scanner(System.in);
+					End = end.next();
+					Scanner email = new Scanner(System.in);
+					while(ok != true){
+						Email = nome.next();
+						Pattern verif = Pattern.compile(["@.|@.com|@.com.|^\b.", pattern.LITERAL]);
+						Matcher compara = verif.matcher(Email);
+						ok = matcher.find();
+						if(ok == false)
+						System.out.println("email invalido");
+					}
+					Scanner pais = new Scanner(System.in);
+					Pais = pais.next();
+					Scanner num_pas = new Scanner(System.in);
+					Num_pas = num_pas.next();
+					Scanner cpf = new Scanner(System.in);
+					Cpf = nome.nextInt();
+					Scanner dt_nasc = new Scanner(System.in);
+					Dt_nasc = nome.nextInt();
+					Scanner cep = new Scanner(System.in);
+					Cep = cep.nextInt();
+					
+				}
+
+			}
+		}
 	}
 
 }
